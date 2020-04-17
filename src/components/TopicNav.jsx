863ced52de7bf8b82c27f44b7055c91d46040bfc
NickOfRhyme <nhwoolrich@gmail.com>
2020-04-17 17:50:27 +0100

    refactor TopicNav component to a function using useState hook

--- src/components/TopicNav.jsx
+++ src/components/TopicNav.jsx
@@ -1,48 +1,39 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { navigate } from "@reach/router";
 import styles from "./css/TopicNav.module.css";
 
-class TopicNav extends Component {
-  state = {
-    textInput: ""
+const TopicNav = () => {
+  const [textInput, setTextInput] = useState("");
+
+  const handleTyping = (event) => {
+    setTextInput(event.target.value);
   };
 
-  render() {
-    const { goToTopic, handleTyping } = this;
-    const { textInput } = this.state;
-
-    return (
-      <nav className={styles.mainNav}>
-        <form className={styles.navContents} onSubmit={goToTopic}>
-          <label className={styles.navLabel}>
-            Choose a topic:
-            <br />
-            <input
-              type="text"
-              name="topic"
-              inputMode="search"
-              value={textInput}
-              onChange={handleTyping}
-            />
-          </label>
-          <button className={styles.navButton}>Take me there!</button>
-        </form>
-        <p className={styles.hint}>
-          Popular topics include cooking, football and coding
-        </p>
-      </nav>
-    );
-  }
-
-  handleTyping = event => {
-    this.setState({ textInput: event.target.value });
-  };
-
-  goToTopic = event => {
+  const goToTopic = (event) => {
     event.preventDefault();
-    const { textInput } = this.state;
-
     navigate(`/topics/${textInput}`);
   };
-}
+
+  return (
+    <nav className={styles.mainNav}>
+      <form className={styles.navContents} onSubmit={goToTopic}>
+        <label className={styles.navLabel}>
+          Choose a topic:
+          <br />
+          <input
+            type="text"
+            name="topic"
+            inputMode="search"
+            value={textInput}
+            onChange={handleTyping}
+          />
+        </label>
+        <button className={styles.navButton}>Take me there!</button>
+      </form>
+      <p className={styles.hint}>
+        Popular topics include cooking, football and coding
+      </p>
+    </nav>
+  );
+};
 export default TopicNav;
