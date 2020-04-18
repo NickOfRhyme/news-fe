90c352331b80108ff45288f5a063ee834197fcc8
NickOfRhyme <nhwoolrich@gmail.com>
2020-04-18 12:08:09 +0100

    refactored Sortbar component into function using useState and useEffect hooks

--- src/components/Sortbar.jsx
+++ src/components/Sortbar.jsx
@@ -1,50 +1,34 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./css/Sortbar.module.css";
 
-class Sortbar extends Component {
-  state = {
-    sort_by: "created_at",
-    order: "desc"
-  };
+const Sortbar = ({ incCommentOption, sortingFunc, topic }) => {
+  const [sort_by, setSort] = useState("created_at");
+  const [order, setOrder] = useState("desc");
 
-  render() {
-    const { handleChange } = this;
-    const { incCommentOption } = this.props;
-    return (
-      <form className={styles.mainSortbar}>
-        <label>
-          Sort by:
-          <select onChange={handleChange} id="" name="sort_by">
-            <option value="created_at">date</option>
-            {incCommentOption && (
-              <option value="comment_count">comments</option>
-            )}
-            <option value="votes">votes</option>
-          </select>
-          <select onChange={handleChange} id="" name="order">
-            <option value="desc">descending</option>
-            <option value="asc">ascending</option>
-          </select>
-        </label>
-      </form>
-    );
-  }
+  useEffect(() => sortingFunc({ topic, order, sort_by }), [order, sort_by]);
 
-  handleChange = event => {
+  const handleChange = (event) => {
     const { value, name } = event.target;
-    this.setState({ [name]: value });
+    if (name === "sort_by") setSort(value);
+    if (name === "order") setOrder(value);
   };
 
-  componentDidUpdate = (prevProps, prevState) => {
-    const { sortingFunc, topic } = this.props;
-    const { sort_by, order } = this.state;
-    const prevSort_by = prevState.sort_by;
-    const prevOrder = prevState.order;
-
-    if (sort_by !== prevSort_by || order !== prevOrder) {
-      sortingFunc({ topic, sort_by, order });
-    }
-  };
-}
+  return (
+    <form className={styles.mainSortbar}>
+      <label>
+        Sort by:
+        <select onChange={handleChange} id="" name="sort_by">
+          <option value="created_at">date</option>
+          {incCommentOption && <option value="comment_count">comments</option>}
+          <option value="votes">votes</option>
+        </select>
+        <select onChange={handleChange} id="" name="order">
+          <option value="desc">descending</option>
+          <option value="asc">ascending</option>
+        </select>
+      </label>
+    </form>
+  );
+};
 
 export default Sortbar;
