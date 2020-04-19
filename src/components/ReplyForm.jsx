45451be0f2725846ca8b83083408fc143a864054
NickOfRhyme <nhwoolrich@gmail.com>
2020-04-19 13:39:08 +0100

    refactored ReplyForm component from class to function using useState and useContext hooks

--- src/components/ReplyForm.jsx
+++ src/components/ReplyForm.jsx
@@ -1,55 +1,43 @@
-import React, { Component } from "react";
+import React, { useState, useContext } from "react";
 import { UserContext } from "./contexts/UserContext";
 import styles from "./css/ReplyForm.module.css";
 
-class ReplyForm extends Component {
-  static contextType = UserContext;
+const ReplyForm = (props) => {
+  const [value, setValue] = useState("");
+  const { user } = useContext(UserContext);
 
-  state = {
-    value: ""
+  const handleChange = (event) => {
+    setValue(event.target.value);
   };
 
-  render() {
-    const { user } = this.context;
-
-    return (
-      <form
-        id="reply-form"
-        onSubmit={this.handleSubmit}
-        className={styles.wholeForm}
-      >
-        <label className={styles.label}>
-          Enter your reply:
-          <textarea
-            required
-            onChange={this.handleChange}
-            value={this.state.value}
-            className={styles.textarea}
-          />
-        </label>
-        <button
-          type="submit"
-          className={styles.postButton}
-          disabled={user === "Guest"}
-        >
-          Post comment
-        </button>
-      </form>
-    );
-  }
-
-  handleChange = (event) => {
-    const value = event.target.value;
-    this.setState({ value });
-  };
-
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const body = this.state.value;
-    const { article_id, addComment, user } = this.props;
+    const body = value;
+    const { article_id, addComment, user } = props;
     addComment(article_id, user, body);
-    this.setState({ value: "" });
+    setValue("");
   };
-}
+
+  return (
+    <form id="reply-form" onSubmit={handleSubmit} className={styles.wholeForm}>
+      <label className={styles.label}>
+        Enter your reply:
+        <textarea
+          required
+          onChange={handleChange}
+          value={value}
+          className={styles.textarea}
+        />
+      </label>
+      <button
+        type="submit"
+        className={styles.postButton}
+        disabled={user === "Guest"}
+      >
+        Post comment
+      </button>
+    </form>
+  );
+};
 
 export default ReplyForm;
